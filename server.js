743f7b3c78ef97723d7bc4e3083e0e7d7ee418f7
Ankit-jailwal <jailwalankit@gmail.com>
2022-01-09 15:02:18 +0530

    Adding post request for creating new users

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 
-const users = []
+app.use(express.json())
+
+const users = [{name:"TestUser"}]
 
 app.get('/users',(req,res) =>{
     res.json(users)
 })
 
+app.post('/users',(req,res)=>{
+    const user = { 
+        name: req.body.name, 
+        password:req.body.password 
+    }
+    users.push(user)
+    res.status(200).send("User was created")
+})
 app.listen(3000)
