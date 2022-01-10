4dc227727528c2fcd24d201fa56676946b428aae
Ankit-jailwal <jailwalankit@gmail.com>
2022-01-10 17:09:54 +0530

    Created userSchema

---
+++ model/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        max: 255,
+        min: 3,
+        required: true
+    },
+    email:{
+        type: String,
+        max: 255,
+        min: 6,
+        required: true
+    },
+    password:{
+        type: String,
+        max: 1024,
+        min: 8,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
