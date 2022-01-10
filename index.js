0b3559adaf53aac4c17084579f38118b435c7e40
Ankit-jailwal <jailwalankit@gmail.com>
2022-01-11 01:55:03 +0530

    Connected mongodb and Joi for validation

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+// Import Routes
+const authRoute = require('./routes/auth')
 
 dotenv.config()
 
 // Connect database
 mongoose.connect( process.env.DB_CONNECT,
     { useNewUrlParser: true },
-    () => console.log("Connected to db!"));
+    () => console.log("Connected to db!"))
 
-// Import Routes
-const authRoute = require('./routes/auth')
+// Middleware
+
+app.use(express.json())
 
 // Route middleware
-app.use('/api/user', authRoute, express.json())
+app.use('/api/user', authRoute)
 
-app.listen(3000, () => console.log("Server up and running"))
+app.listen(4000, () => console.log("Server up and running"))
--- routes/auth.js
+++ routes/auth.js
@@ -1,44 +1,65 @@
-const router = require('express').Router();
+const router = require('express').Router()
+const User = require('../model/User')
 const bcrypt = require('bcrypt')
+const Joi = require('@hapi/joi');
 
-const users = [{ name: "TestUser" }]
+// Validation
+
+const schema = Joi.object({
+    name: Joi.string()
+        .min(3)
+        .required()
+        .max(255),
+    email: Joi.string()
+        .min(6)
+        .max(255)
+        .required()
+        .email(),
+    password: Joi.string()
+        .min(8)
+        .max(1024)
+        .required(),
+
+})
 
 router.get('/users', (req, res) => {
-    res.json(users)
+    res.json("Hello world")
 })
 
 router.post('/register', async (req, res) => {
+
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
     try {
-        // const salt = await bcrypt.genSalt()
-        // const hashPassword = await bcrypt.hash(req.body.password, salt)
-        // const user = {
-        //     name: req.body.name,
-        //     password: hashPassword
-        // }
-        // users.push(user)
-        res.status(200).send("User was created")
+        const saveUser = await user.save()
+        res.send(saveUser)
     }
-    catch {
-        res.status(500).send("Something went wrong!")
+    catch (err) {
+        res.status(400).send(err)
     }
 })
 
-router.post('/login', async (req, res) => {
-    // const user = users.find(user => user.name === req.body.name)
-    // if (user == null) {
-    //     return res.status(400).send("Cannot find user")
-    // }
-    // try {
-    //     if (await bcrypt.compare(req.body.password, user.password)) {
-    //         res.send("Success")
-    //     }
-    //     else {
-    //         res.send("Not allowed")
-    //     }
-    // }
-    // catch {
-    //     res.status(500).send("Something went wrong!")
-    // }
-    res.status(200).send("Working fine!")
-})
+// router.post('/login', async (req, res) => {
+//     // const user = users.find(user => user.name === req.body.name)
+//     // if (user == null) {
+//     //     return res.status(400).send("Cannot find user")
+//     // }
+//     // try {
+//     //     if (await bcrypt.compare(req.body.password, user.password)) {
+//     //         res.send("Success")
+//     //     }
+//     //     else {
+//     //         res.send("Not allowed")
+//     //     }
+//     // }
+//     // catch {
+//     //     res.status(500).send("Something went wrong!")
+//     // }
+//     res.status(200).send("Working fine!")
+// })
 module.exports = router
